edb4eb2163ade7931a7e84b4f1d016e2fd0cf46e
Jaldeep Acharya <jka67@cornell.edu>
2016-10-02 21:01:55 +0000

    Finished script

--- pytree.py
+++ pytree.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
+
+noOfFiles = 0
+noOfFolders = 0
 
 
-# YOUR CODE GOES here
+# Recursively print the tree
+def tree(currDir, printOffset):
+    global noOfFiles
+    global noOfFolders
 
+    dirContent = [cont for cont in os.listdir(currDir) if not cont.startswith('.')]
+    dirContentSorted = sorted(dirContent, key=lambda s: s.lower())
 
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    # Loop through the content
+    i = 0
+    for content in dirContentSorted:
+        # If it's a file and not the last one, just print it with the offset
+        if(os.path.isfile(os.path.join(currDir, content)) and i < len(dirContentSorted) - 1):
+            print(printOffset + "├── " + str(content))
+            noOfFiles += 1
+
+        # If it is the last file in the current directory
+        elif(os.path.isfile(os.path.join(currDir, content)) and i == len(dirContentSorted) - 1):
+            print(printOffset + "└── " + str(content))
+            noOfFiles += 1
+
+        # If it's a folder, then print the name of the folder with offset, and then recurse
+        elif(os.path.isdir(os.path.join(currDir, content)) and i < len(dirContentSorted) - 1):
+            print(printOffset + "├── " + str(content))
+            noOfFolders += 1
+            tree(os.path.join(currDir, content), printOffset + "│   ")
+
+        # If it is the last folder in the current directory
+        elif(os.path.isdir(os.path.join(currDir, content)) and i == len(dirContentSorted) - 1):
+            print(printOffset + "└── " + str(content))
+            noOfFolders += 1
+            tree(os.path.join(currDir, content), printOffset + "    ")
+
+        i += 1
+
+# Print the first line
+lenArg = len(sys.argv)
+if(lenArg == 1):
+    rootDir = "."
+else:
+    rootDir = str(sys.argv[1])
+print(rootDir)
+tree(rootDir, "")
+print()
+print(str(noOfFolders) + " directories, " + str(noOfFiles) + " files")
